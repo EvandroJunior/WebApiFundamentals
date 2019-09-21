d3136d6b504db807992cedb925d6bfa798179eba
EvandroJnr <efgjunior@gmail.com>
2019-09-21 00:21:12 -0300

    Add Automapper to the project. Add mapping from Camp to CampModel. Camps/Get now returns CampModels instead of Camps.

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -1,20 +1,25 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using TheCodeCamp.Data;
+using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
     public class CampsController : ApiController
     {
         private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
 
-        public CampsController(ICampRepository repository)
+        public CampsController(ICampRepository repository,
+            IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         public async Task<IHttpActionResult> Get()
@@ -23,12 +28,15 @@ namespace TheCodeCamp.Controllers
             {
                 var result = await _repository.GetAllCampsAsync();
 
-                return Ok(result);
+                // Mapping
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+                
+                return Ok(mappedResult);
             }
             catch (Exception ex)
             {
                 // TODO Add Logging
-                return InternalServerError();
+                return InternalServerError(ex);
             }
         }
     }
