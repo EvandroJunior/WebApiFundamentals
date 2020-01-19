11bcb4eae8c7f661c2241b5d30be39c9364a9101
EvandroJnr <efgjunior@gmail.com>
2020-01-18 23:39:47 -0300

    Add the approach to request api version from the url path.

--- starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Web.Http;
+using Microsoft.Web.Http.Routing;
 using Microsoft.Web.Http.Versioning;
 using Newtonsoft.Json.Serialization;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace TheCodeCamp
 {
@@ -17,17 +19,28 @@ namespace TheCodeCamp
                 cfg.DefaultApiVersion = new ApiVersion(1, 1);
                 cfg.AssumeDefaultVersionWhenUnspecified = true;
                 cfg.ReportApiVersions = true;
-                cfg.ApiVersionReader = ApiVersionReader.Combine(
-                    new HeaderApiVersionReader("X-Version"),
-                    new QueryStringApiVersionReader("ver"));
+                cfg.ApiVersionReader = new UrlSegmentApiVersionReader();
+                //ApiVersionReader.Combine(
+                //    new HeaderApiVersionReader("X-Version"),
+                //    new QueryStringApiVersionReader("ver"));
             });
 
             // Change Case of Json
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
                 new CamelCasePropertyNamesContractResolver();
 
+            // Adding a way to resolve the variable "version:apiVersion" used on controller routes
+            // to get the request specified version of the API
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap =
+                {
+                    ["apiVersion"] = typeof(ApiVersionRouteConstraint)
+                }
+            };
+
             // Web API routes
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes(constraintResolver);
 
             //config.Routes.MapHttpRoute(
             //    name: "DefaultApi",
